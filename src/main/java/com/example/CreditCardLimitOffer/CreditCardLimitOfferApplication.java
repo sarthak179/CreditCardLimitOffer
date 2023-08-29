c5d1eebadcf7b057526a670bcd2ced5dff132568
Sarthak Gupta <sarthak@Sarthaks-MacBook-Air.local>
2023-08-29 13:17:36 +0530

    changed class name

--- src/main/java/com/example/CreditCardLimitOffer/CreditCardLimitOfferApplication.java
+++ src/main/java/com/example/CreditCardLimitOffer/CreditCardLimitOfferApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SarthakGuptaAssignmentVegaPayApplication {
+public class CreditCardLimitOfferApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SarthakGuptaAssignmentVegaPayApplication.class, args);
+		SpringApplication.run(CreditCardLimitOfferApplication.class, args);
 	}
 
 }
